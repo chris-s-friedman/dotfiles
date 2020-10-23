dea83372415853702793971ae59c1ebeeba69725
Chris Friedman <chris.s.friedman@gmail.com>
2020-10-23 09:27:11 -0400

    :sparkles: vscode dotfiles

---
+++ dotfiles/VSCode/symlink.sh
@@ -0,0 +1,3 @@
+ln -s ~/dotfiles/dotfiles/VSCode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+ln -s ~/dotfiles/dotfiles/VSCode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+ln -s ~/dotfiles/dotfiles/VSCode/snippets/ ~/Library/Application\ Support/Code/User/
