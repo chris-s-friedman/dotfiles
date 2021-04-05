77340bc3f43a22dad023ae952478561dfda842e4
Chris Friedman <chris.s.friedman@gmail.com>
2021-04-05 14:36:55 -0400

    :sparkles: script for seting up basic things for shell

--- system/setup.sh
+++ system/setup.sh
@@ -3,21 +3,13 @@ chsh -s /bin/zsh
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-cp dotfiles/dotfiles/.zshrc .zshrc
+cp ~/dotfiles/dotfiles/.zshrc ~/.zshrc
+cp ~/dotfiles/dotfiles/.p10k.zsh ~/.p10k.zsh
+cp ~/dotfiles/dotfiles/env.sh ~/env.sh
+cp ~/dotfiles/dotfiles/functions.sh ~/functions.sh 
 
 # install oh-my-zsh addons
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
-# if mac install Brew
-
-# Development
-## Install Git
-
-# Install gitmoji
-## if Macos:
-brew install gitmoji
-## If ubuntu
-### If no NPM
