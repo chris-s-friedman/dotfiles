05b139646091e2f1d91bf0b1f4b7bf40ae4a0125
Chris Friedman <chris.s.friedman@gmail.com>
2021-04-05 14:13:41 -0400

    :wrench: adds p10k stuff

--- system/setup.sh
+++ system/setup.sh
@@ -1,6 +1,16 @@
 # Set ZSH as the default shell
 chsh -s /bin/zsh
 
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+cp dotfiles/dotfiles/.zshrc .zshrc
+
+# install oh-my-zsh addons
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
 # if mac install Brew
 
 # Development
