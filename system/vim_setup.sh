3e9f82da6611c0c4563fbeff29b5adea4a9fa2e8
Chris Friedman <chris.s.friedman@gmail.com>
2021-10-14 22:16:15 -0400

    :sparkles: add vim setup script

---
+++ system/vim_setup.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env
+
+set -euo pipefail
+
+cp ~/dotfiles/dotfiles/.vimrc ~/.vimrc 
+
+# Install vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+
+# Install things in os specific manner:
+if [ "$(uname)" == "Darwin" ]
+then
+    # Install Brew
+    brew install macvim cmake python3 mono go nodejs
+    cd ~/.vim/bundle/YouCompleteMe
+    /usr/local/bin/python3 install.py --all
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]
+then
+    # Do something under GNU/Linux platform
+    sudo apt install build-essential cmake vim-nox python3-dev
+    apt install mono-complete golang nodejs default-jdk 
+    cd ~/.vim/bundle/YouCompleteMe
+    python3 install.py --all
+fi
